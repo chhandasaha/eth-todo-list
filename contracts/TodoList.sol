8bf530781026affe0415ab571a461c6656c3f1ad
Chhanda Saha <chhandasaha98@gmail.com>
2022-02-04 19:44:14 +0530

    1. Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16<0.9.0;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+}
---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  networks: {
+    development: {
+      //here the development network is connected to ganach network
+      host: "127.0.0.1",
+      port: 8545, //localhost port, according to utube vdo port was 7545
+      network_id: "*" //match any network id
+    }
+  },
+  // Configure your compilers
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
